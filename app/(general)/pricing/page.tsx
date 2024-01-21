83854c82c6a490ed481b36a1047faf7e9ddaf849
JM Lugo <lugoc.jose@outlook.com>
2024-01-21 15:50:42 -0700

    First Commit

---
+++ app/(general)/pricing/page.tsx
@@ -0,0 +1,14 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Pricing",
+  description: "My Pricing's Page",
+};
+
+export default function PricingPage() {
+  return (
+    <>
+      <span className="text-7xl">Pricing Page</span>
+    </>
+  );
+}
